b3e118d37b3e96e6082dc001b6f7c9192c1ad3d9
divyaanshu <divyanshuwork03@gmail.com>
2026-01-10 01:01:17 +0530

    09-01-2026

--- IP/rab_game_1.cpp
+++ IP/rab_game_1.cpp
@@ -17,37 +17,68 @@ template <typename T> istream &operator>>(istream &is, vector<T> &v) { for (auto
 const int MOD = 1e9 + 7;
 #define nl "\n"
     
+void solve() {
+    int n, a, b;
+    cin >> n >> a >> b;
 
-void solve(){
-    int n,a,b;
-    cin>>n>>a>>b;
-    if(a+b>n){
-        cout<<"NO"<<nl;
+    // A permutation with wins for only one player is impossible.
+    // Every cycle in a permutation of length >= 2 must contain 
+    // at least one increase and one decrease.
+    if (a + b > n || (a > 0 && b == 0) || (b > 0 && a == 0)) {
+        cout << "NO" << nl;
         return;
     }
-    if(a==0 and b==0){
-        cout<<"YES"<<nl;
-        int x=2;
-        while(x--){
-            for(int i=1;i<=n;i++) cout<<i;
-            cout<<nl;
-        }
-        return;
+
+    cout << "YES" << nl;
+
+    vi v1(n), v2(n);
+    for (int i = 0; i < n; i++) {
+        v1[i] = v2[i] = i + 1;
     }
-    int same=n-(a+b);
-    vector <int> arr,brr;
-    for(int i=1;i<=same;i++){
-        arr.pb(i);
-        brr.pb(i);
+
+    // If there are rounds that aren't draws, we take a block of 
+    // size (a+b) and rotate it. Rotating by 'a' positions left 
+    // ensures exactly 'a' elements are smaller than their original 
+    // values and 'b' elements are larger.
+    if (a + b > 0) {
+        rotate(v2.begin(), v2.begin() + a, v2.begin() + a + b);
     }
-    if(a>b){
-        for(int i=n;i>same;i--){
-            arr.pb(i);
-            brr.pb(i-1);
-        }
-        arr.pb()
-    }
+
+    cout << v1 << nl;
+    cout << v2 << nl;
 }
+// void solve(){
+//     int n,a,b;
+//     cin>>n>>a>>b;
+//     if(a+b>n){
+//         cout<<"NO"<<nl;
+//         return;
+//     }
+//     cout<<"YES"<<nl;
+
+//     vector <int> arr,brr;
+//     int low=1, high=n;
+
+//     //player a wins 
+//     for(int i=0;i<a;i++){
+//         arr.pb(high--);
+//         brr.pb(low++);
+//     }
+
+//     //player b wins 
+//     for(int i=0;i<b;i++){
+//         arr.pb(low++);
+//         brr.pb(high--);
+//     }
+
+//     while(low<=high){
+//         arr.pb(low);
+//         brr.pb(low);
+//         low++;
+//     }
+//     cout<<arr<<nl;
+//     cout<<brr<<nl;
+// }
 
 
 signed main() {
